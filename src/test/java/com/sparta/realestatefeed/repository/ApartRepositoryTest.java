2e390a228ecadb4e7ebfbefb51bf4b50d43f8ab4
Hyeonjin Kim <andrew75313@gmail.com>
2024-07-04 20:14:17 +0900

    test(repository): ApartRepositoryTest의 특정 사용자들이 작성한 아파트 게시글 모두 조회 테스트 추가
    -findByFollowingUsers 메서드 테스트

--- src/test/java/com/sparta/realestatefeed/repository/ApartRepositoryTest.java
+++ src/test/java/com/sparta/realestatefeed/repository/ApartRepositoryTest.java
@@ -5,6 +5,7 @@ import com.sparta.realestatefeed.entity.Apart;
 import com.sparta.realestatefeed.entity.User;
 import com.sparta.realestatefeed.entity.UserRoleEnum;
 import com.sparta.realestatefeed.repository.apart.ApartRepository;
+import com.sparta.realestatefeed.repository.like.LikeRepository;
 import com.sparta.realestatefeed.repository.user.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -16,6 +17,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.util.ReflectionTestUtils;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -34,6 +36,8 @@ public class ApartRepositoryTest {
 
     private Apart apart;
     private User user;
+    @Autowired
+    private LikeRepository likeRepository;
 
     private Apart setTestApart(String apartName) {
 
@@ -139,5 +143,50 @@ public class ApartRepositoryTest {
         assertEquals(apartList.get(2).getArea(), "Seoul");
         assertEquals(apartList.get(3).getArea(), "Seoul");
         assertEquals(apartList.get(4).getArea(), "Seoul");
+    }
+
+    @Test
+    @DisplayName("특정 사용자들이 작성한 아파트 게시글 모두 조회")
+    void testFindByFollowingUsers() {
+        // given
+        List<Long> followingIdList = new ArrayList<>();
+
+        User testUser1 = setTestUser("testUser1");
+        userRepository.save(testUser1);
+
+        Long testUser1Id = testUser1.getId();
+        followingIdList.add(testUser1Id);
+
+        for (int apartIndex = 1; apartIndex <= 3; apartIndex++) {
+            apart = setTestApart("seoulapart" + apartIndex);
+            ReflectionTestUtils.setField(apart, "area", "Seoul");
+            ReflectionTestUtils.setField(apart, "user", testUser1);
+            apartRepository.save(apart);
+        }
+
+        User testUser2 = setTestUser("testUser2");
+        userRepository.save(testUser2);
+
+        Long testUser2Id = testUser2.getId();
+        followingIdList.add(testUser2Id);
+
+        for (int apartIndex = 1; apartIndex <= 3; apartIndex++) {
+            apart = setTestApart("seoulapart" + apartIndex);
+            ReflectionTestUtils.setField(apart, "area", "Seoul");
+            ReflectionTestUtils.setField(apart, "user", testUser2);
+            apartRepository.save(apart);
+        }
+
+        // when
+        List<Apart> apartList = apartRepository.findByFollowingUsers(followingIdList, 0);
+
+        // then
+        assertFalse(apartList.isEmpty());
+        assertEquals(5, apartList.size());
+        assertEquals(apartList.get(0).getUser().getId(), testUser2Id);
+        assertEquals(apartList.get(1).getUser().getId(), testUser2Id);
+        assertEquals(apartList.get(2).getUser().getId(), testUser2Id);
+        assertEquals(apartList.get(3).getUser().getId(), testUser1Id);
+        assertEquals(apartList.get(4).getUser().getId(), testUser1Id);
     }
 }
