4a9ee25fc2e2b4492ba19e18db3b42bf239f28ed
Hyeonjin Kim <andrew75313@gmail.com>
2024-07-04 14:44:40 +0900

    test(repository): QnARepositoryTest의 특정 문의 댓글 조회 테스트 추가
    -findByQnaId 메서드 테스트
    -성공/실패 테스트 포함

---
+++ src/test/java/com/sparta/realestatefeed/repository/QnARepositoryTest.java
@@ -0,0 +1,84 @@
+package com.sparta.realestatefeed.repository;
+
+import com.sparta.realestatefeed.config.TestConfig;
+import com.sparta.realestatefeed.entity.QnA;
+import com.sparta.realestatefeed.entity.User;
+import com.sparta.realestatefeed.repository.qna.QnARepository;
+import com.sparta.realestatefeed.repository.user.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@ActiveProfiles("test")
+@Import(TestConfig.class)
+public class QnARepositoryTest {
+
+    @Autowired
+    private QnARepository qnARepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private QnA qna;
+    private User user;
+
+    private QnA setTestQna(String content) {
+
+        QnA qna = new QnA();
+        ReflectionTestUtils.setField(qna, "content", content);
+        ReflectionTestUtils.setField(qna, "isCompleted", false);
+
+        return qna;
+    }
+
+    @BeforeEach
+    void setUp() {
+        qna = new QnA();
+        user = new User();
+    }
+
+    @Nested
+    @DisplayName("특정 문의 댓글 조회")
+    class FindByQnaIdTest {
+
+        @Test
+        @DisplayName("특정 문의 댓글이 존재할 경우")
+        void testFindByQnaIdSuccess() {
+            // given
+            qna = setTestQna("testcontents");
+            qnARepository.save(qna);
+
+            // when
+            Optional<QnA> optionalQnA = qnARepository.findByQnaId(qna.getQnaId());
+
+            // then
+            assertTrue(optionalQnA.isPresent());
+            assertEquals(qna.getQnaId(), optionalQnA.get().getQnaId());
+        }
+
+        @Test
+        @DisplayName("특정 문의 댓글이 없을 경우")
+        void testFindByQnaIdFailure() {
+            // given
+            qna = setTestQna("testcontents");
+            qnARepository.save(qna);
+
+            // when
+            Optional<QnA> optionalQnA = qnARepository.findByQnaId(999L);
+
+            // then
+            assertFalse(optionalQnA.isPresent());
+        }
+    }
+}
